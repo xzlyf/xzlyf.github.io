cc84cc4ac316cd4d380215a5640d27cd9d4bc539
xzlyf <czr2001@outlook.com>
2019-05-23 12:45:08 +0800

    大更新

---
+++ js/tips.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function rec(){
+    var mychar="-动态-功能暂时停机维护，恢复时间未知";
+    alert(mychar);
+  }
+  function rec_1(){
+    var mychar="-关于-功能暂时停机维护，恢复时间未知";
+    alert(mychar);
+  }
